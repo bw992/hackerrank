7c35755e59e04e2b6a9a8f091356ef044535f06a
Bowen Wang <bowenwang@Bowens-MacBook.home>
2017-01-29 02:04:33 -0500

    " first day of school"

---
+++ hackerrankday12.java
@@ -0,0 +1,73 @@
+
+import java.util.*;
+
+class Person {
+  protected String firstName;
+  protected String lastName;
+  protected int idNumber;
+  
+  // Constructor
+  Person(String firstName, String lastName, int identification){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.idNumber = identification;
+  }
+  
+  // Print person data
+  public void printPerson(){
+     System.out.println(
+        "Name: " + lastName + ", " + firstName 
+      +   "\nID: " + idNumber); 
+  }
+   
+}
+
+class Student extends Person{
+	private int[] testScores;
+   
+    Student(String firstName, String lastName, int id, int[] testScores){
+      // Set pointer to new parameters
+      super(firstName,lastName,id);
+      this.testScores=testScores;
+      this.firstName=firstName;
+      this.lastName=lastName;
+      this.idNumber=id;
+    }
+   // Method without argument 
+   //
+    public  char calculate() {
+            int sum =0;
+            for (int i=0;i<testScores.length;i++){
+                sum += testScores[i];
+            }
+			int avg = sum/ testScores.length;
+           
+             if(avg < 40) return 'T';
+        else if(avg < 55) return 'D';
+        else if(avg < 70) return 'P';
+        else if(avg < 80) return 'A';
+        else if(avg < 90) return 'E';
+        else return 'O';
+            
+		}
+  	  	
+}
+
+class Solution {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String firstName = scan.next();
+    String lastName = scan.next();
+    int id = scan.nextInt();
+    int numScores = scan.nextInt();
+    int[] testScores = new int[numScores];
+    for(int i = 0; i < numScores; i++){
+      testScores[i] = scan.nextInt();
+    }
+    scan.close();
+    
+    Student s = new Student(firstName, lastName, id, testScores);
+    s.printPerson();
+    System.out.println("Grade: " + s.calculate());
+  }
+}
